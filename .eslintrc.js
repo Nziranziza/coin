9b65ea1f7757bd6a322fb70549e34833373f7885
nziranziza <nziranzizadaniel@gmail.com>
2024-06-07 17:36:04 +0200

    Configure eslint and husky

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ["expo", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "no-console": "warn",
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+    "eol-last": ["error", "always"],
+  },
+};
